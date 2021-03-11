431eefb93443887ced6e47b185b1057856b12bf6
Roopkishor Singh <rks@Roopkishors-MacBook-Pro.local>
2021-03-11 20:58:27 +0530

    flask setup

--- app/api.py
+++ app/api.py
@@ -1,4 +1,17 @@
+"""
 # here starts the journey of web app
 1. Make a simple login website
+"""
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
 
